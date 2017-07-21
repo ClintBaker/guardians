820dcd2b62e4eed2e748a244ce87e627811ca509
Clint Baker <clintonjamesbaker@gmail.com>
2017-07-20 18:36:46 -0600

    init commit

---
+++ app/components/Landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Landing extends React.Component {
+  render () {
+    return (
+      <div className="small-centered small-10 medium-6 large-4 center">
+        <div className="callout primary">
+          <h5>Welcome to Spirit Animal.</h5>
+          <p>Embrace your inner spirit animal!</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Landing;
